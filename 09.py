edd51c6c1dd6b737da3a704958792e78b811262c
jeremy0123 <hallelujah0123@163.com>
2014-12-02 14:58:08 +0800

    内置函数

---
+++ 09.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+# python2.x的内置函数
+print abs(100)
+print abs(-20)
+print abs(12.34)
+
+print cmp(1, 2)
+print cmp(2, 1)
+print cmp(3, 3)
+
+print int('123')
+print int(12.34)
+print float('12.34')
+print str(1.23)
+print unicode(100)
+print bool(1)
+print bool('')
+
+a = abs
+print a(-1)
