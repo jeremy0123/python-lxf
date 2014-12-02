67e5062ed576cc6d025623c69fe4c980e2dd4ab9
jeremy0123 <hallelujah0123@163.com>
2014-12-02 11:05:20 +0800

    添加02.py

---
+++ 02.py
@@ -0,0 +1,8 @@
+print 'the quick brown fox', 'jumps over', 'the lazy dog'
+print 300
+print 100 + 200
+print '100 + 200 =', 100 + 200
+name = raw_input()
+print 'hello', name
+name = raw_input("please enter your name: ")
+print 'hello', name
