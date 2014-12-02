2a8b564a8e793de5a005acb4203d992ed0ccb7b5
jeremy0123 <hallelujah0123@163.com>
2014-12-02 13:41:01 +0800

    格式化字符串

--- 05.py
+++ 05.py
@@ -21,3 +21,9 @@ print 'abc'.decode('utf-8')
 print '\xe4\xb8\xad\xe6\x96\x87'.decode('utf-8') 
 #'\xe4\xb8\xad\xe6\x96\x87'.decode('utf-8') >>> u'\u4e2d\u6587'
 
+# 格式化字符串
+print 'Hi, %s, you have $%d.' % ('Michael', 1000000)
+# Unicode 前后统一
+print u'Hi, %s' % u'Michale'
+
+# 在Python 3.x版本中，把'xxx'和u'xxx'统一成Unicode编码，即写不写前缀u都是一样的，而以字节形式表示的字符串则必须加上b前缀：b'xxx'。
