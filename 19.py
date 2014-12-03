f97f251dbf68c95444a47eb7c91a2697171f2f06
jeremy0123 <hallelujah0123@163.com>
2014-12-03 16:06:05 +0800

    装饰器 @的使用

---
+++ 19.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+
+# 装饰器 decorator
+
+def now():
+    print '2014-12-03'
+
+f = now
+f()
+# 函数也是一个对象 函数对象有一个__name__属性
+print now.__name__
+print f.__name__
+
+# ------------------------------------------------------------------------------
+# 在代码运行期间动态增加功能的方式，称之为“装饰器”（Decorator）。
+def log(func):
+    def wrapper(*args, **kw):
+        print 'call %s():' % func.__name__
+        return func(*args, **kw)
+    return wrapper
+
+@log # 等价于 now = log(now)
+def now():
+    print '2014-12-03'
+
+now()
+print now.__name__ # wrapper 已经改变了属性，需要改回来
+
+# ------------------------------------------------------------------------------
+def log(text):
+    def decorator(func):
+        def wrapper(*args, **kw):
+            print '%s %s():' % (text, func.__name__)
+            return func(*args, **kw)
+        return wrapper
+    return decorator
+
+@log('exectute') #等价于 now = log('execute')(now)
+def now():
+    print '2014-12-03'
+
+now()
+print now.__name__ # wrapper 已经改变了属性，需要改回来
+
+# ------------------------------------------------------------------------------
+import functools
+
+def log(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kw):
+        print 'call %s():' % func.__name__
+        return func(*args, **kw)
+    return wrapper
+@log # 等价于 now = log(now)
+def now():
+    print '2014-12-03'
+now()
+print now.__name__ # now
+
+# ------------------------------------------------------------------------------
+def log(text):
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kw):
+            print '%s %s():' % (text, func.__name__)
+            return func(*args, **kw)
+        #wrapper.__name__ = func.__name__
+        return wrapper
+    return decorator
+
+@log('exectute') #等价于 now = log('execute')(now)
+def now():
+    print '2014-12-03'
+now()
+print now.__name__ # now
