5514fe5b3992078f377c3663aa0b4368f14d259c
jeremy0123 <hallelujah0123@163.com>
2014-12-03 13:37:15 +0800

    list tuple string的切片

---
+++ 13.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+# 切片 slice
+L = ['Michael', 'Sarah', 'Tracy', 'Bob', 'Jack']
+print L[0:3] # [0 - 3)
+print L[3:] # [3 - end]
+print L[:3] # [begin, 3)
+print L[-2:] # [-2 - end]
+
+L = range(100)
+print L
+print L[:10]
+print L[10:20]
+print L[-10:]
+print L[:10:2] # 前10个数，每2个取一个
+print L[::5] # 所有数，每5个取一个
+print L[:]
+
+# tuple 和 string 都可以看做list，进行切片操作
