1608302ddf66f4ac10e908a7edaf6ac9d9de3eb7
jeremy0123 <hallelujah0123@163.com>
2014-12-03 13:46:55 +0800

    迭代，下标，判断可迭代

---
+++ 14.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+# 迭代 itervalues() iteritems()
+d = {
+    'a': 1,
+    'b': 2,
+    'c': 3
+}
+
+print d
+for key in d:
+    print key
+
+for value in d.itervalues():
+    print value
+
+for k, v in d.iteritems():
+    print k, "=", v
+
+# str也可以迭代
+
+# 判断一个对象是否是可迭代的，通过collections的Iterable类型判断
+from collections import Iterable
+print isinstance('abc', Iterable)
+print isinstance([1, 2, 3], Iterable)
+print isinstance(123, Iterable)
+
+# 下标循环 enumerate吧list变成索引-元素对
+for i, value in enumerate(['A', 'B', 'C']):
+    print i, value
