0ee02723d210521e221a204b01a66b0cc9e7d39e
jeremy0123 <hallelujah0123@163.com>
2014-12-03 10:01:49 +0800

    函数参数02

--- 11.py
+++ 11.py
@@ -1,6 +1,11 @@
 # -*- coding: utf-8 -*-
 
-def power(x):
-    return x * x
+def power(x, n = 2):
+    s = 1
+    while n > 0:
+        n = n - 1
+        s = s * x
+    return s
 
 print power(5)
+print power(10, 3)
