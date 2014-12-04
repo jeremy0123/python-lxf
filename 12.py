00b161ca66a782ceb5facf315dd2ee4fb6ab15cb
jeremy0123 <hallelujah0123@163.com>
2014-12-04 10:08:09 +0800

    尾递归优化测试

--- 12.py
+++ 12.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+# 函数递归
 def fact(n):
     if n == 1:
         return 1
@@ -31,3 +32,53 @@ def fact_iter(product, count, max):
 
 print fact(5)
 # print fact(1000) # python并未做尾递归优化
+
+# 下列代码可以进行优化
+# This program shows off a python decorator
+# which implements tail call optimization. It
+# does this by throwing an exception if it is
+# it's own grandparent, and catching such
+# exceptions to recall the stack.
+
+import sys
+
+class TailRecurseException:
+  def __init__(self, args, kwargs):
+    self.args = args
+    self.kwargs = kwargs
+
+def tail_call_optimized(g):
+  """
+  This function decorates a function with tail call
+  optimization. It does this by throwing an exception
+  if it is it's own grandparent, and catching such
+  exceptions to fake the tail call optimization.
+  This function fails if the decorated
+  function recurses in a non-tail context.
+  """
+  def func(*args, **kwargs):
+    f = sys._getframe()
+    if f.f_back and f.f_back.f_back \
+        and f.f_back.f_back.f_code == f.f_code:
+      raise TailRecurseException(args, kwargs)
+    else:
+      while 1:
+        try:
+          return g(*args, **kwargs)
+        except TailRecurseException, e:
+          args = e.args
+          kwargs = e.kwargs
+  func.__doc__ = g.__doc__
+  return func
+
+def fact(n):
+    return fact_iter(1, 1, n)
+
+@tail_call_optimized
+def fact_iter(product, count, max):
+    if count > max:
+        return product
+    return fact_iter(product * count, count + 1, max)
+
+print fact(5)
+print fact(1000) #Welldone
