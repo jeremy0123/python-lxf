66e964002a2d1b03932085465311e0f3175e3e06
jeremy0123 <hallelujah0123@163.com>
2014-12-11 10:04:36 +0800

    flask实现webserver

--- 66.py
+++ 66.py
@@ -1,8 +1,27 @@
-# -*-coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 
-from wsgiref.simple_server import make_server
-from app import application
+from flask import Flask
+from flask import request
 
-httpd = make_server('', 8000, application)
-print 'Server HTTP on port 8000'
-httpd.serve_forever()
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    return '<h1>Home</h1>'
+
+@app.route('/signin', methods=['GET'])
+def signin_form():
+    return '''<form action="/signin" method="post">
+              <p><input name="username" /></p>
+              <p><input name="password" type="password" /></p>
+              <p><button type="submit">Sign In</button></p>
+              </form>'''
+
+@app.route('/signin', methods=['POST'])
+def signin():
+    if request.form['username'] == 'admin' and request.form['password'] == 'password':
+        return '<h3>Hello, admin!</h3>'
+    return '<h3>Bad username or password.</h3>'
+
+if __name__ == '__main__':
+    app.run()
